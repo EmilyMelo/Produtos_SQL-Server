d2631b30d7cc960d17dd1ec7dce4828d63d79c1f
EmilyMelo <melo.emily1999@gmail.com>
2022-07-28 21:55:45 -0300

    Atualizando estrutura de arquivos.

--- SCRIPTS/002_SCRIPT_INSERT_PRODUTOS.sql
+++ SCRIPTS/002_SCRIPT_INSERT_PRODUTOS.sql
@@ -1,1 +1,9 @@
-/*inserindo dados na tabela tbProdutos*/
+/*Inserindo dados na tabela tbProdutos*/
+
+INSERT INTO tb_Produtos (
+	nmProduto,
+	qtdProduto,
+	dtVencimento,
+	idFuncionario
+)
+VALUES('beterraba', 15, '2022-08-07', 1000001)
--- SCRIPTS/INSERT_PRODUTOS.sql
+++
@@ -1,9 +0,0 @@
-/*Inserindo dados na tabela tbProdutos*/
-
-INSERT INTO tb_Produtos (
-	nmProduto,
-	qtdProduto,
-	dtVencimento,
-	idFuncionario
-)
-VALUES('beterraba', 15, '2022-08-07', 1000001)
